7db2741399641a1407723688eb71f9262e808b1b
Linus Wigger <wiggerl@student.ethz.ch>
2020-04-16 07:15:15 +0200

    One more particle tracing function

--- core/ParticleTracer.cpp
+++ core/ParticleTracer.cpp
@@ -2,7 +2,7 @@
 
 
 
-Vec3f traceParticle(RegVectorField3f field, Vec3f start, float dt) {
+Vec3f traceParticle(const RegVectorField3f& field, Vec3f start, float dt) {
 	Vec3f k1 = field.Sample(start);
 	Vec3f k2 = field.Sample(start + k1 * dt*0.5f);
 	Vec3f k3 = field.Sample(start + k2 * dt*0.5f);
@@ -10,10 +10,21 @@ Vec3f traceParticle(RegVectorField3f field, Vec3f start, float dt) {
 	return Vec3f(start + (k1 + k2 * 2 + k3 * 2 + k4) * (dt / 6.f));
 }
 
-Vec3f traceParticle(RegVectorField3f field0, RegVectorField3f field1, Vec3f start, float dt){
+Vec3f traceParticle(const RegVectorField3f& field0, const RegVectorField3f& field1, Vec3f start, float dt){
 	Vec3f k1 = field0.Sample(start);
 	Vec3f k2 = (field0.Sample(start + k1 * dt*0.5f) + field1.Sample(start + k1 * dt*0.5f)) *0.5f;
 	Vec3f k3 = (field0.Sample(start + k2 * dt*0.5f) + field1.Sample(start + k2 * dt*0.5f)) *0.5f;
 	Vec3f k4 = field1.Sample(start + k3 * dt);
+	return Vec3f(start + (k1 + k2 * 2 + k3 * 2 + k4) * (dt / 6.f));
+}
+
+Vec3f traceParticle(const RegVectorField3f& field0, float t0, const RegVectorField3f& field1, float t1, Vec3f start, float start_t, float dt) {
+	float t_a = start_t;
+	float t_b = start_t + 0.5f*dt;
+	float t_c = start_t + dt;
+	Vec3f k1 = field0.Sample(start) * (1 - t_a) + field1.Sample(start) * t_a;
+	Vec3f k2 = field0.Sample(start + k1 * dt*0.5f) * (1 - t_b) + field1.Sample(start + k1 * dt*0.5f) * t_b;
+	Vec3f k3 = field0.Sample(start + k2 * dt*0.5f) * (1 - t_b) + field1.Sample(start + k2 * dt*0.5f) * t_b;
+	Vec3f k4 = field0.Sample(start) * (1 - t_c) + field1.Sample(start) * t_c;
 	return Vec3f(start + (k1 + k2 * 2 + k3 * 2 + k4) * (dt / 6.f));
 }
--- core/ParticleTracer.hpp
+++ core/ParticleTracer.hpp
@@ -5,6 +5,8 @@
 #include "RegularGrid.hpp"
 
 //float, 3d, time invariant
-Vec3f traceParticle(RegVectorField3f field, Vec3f start, float dt);
-//float, 3d, field0 at time t0, field1 at time to+dt, linear interpolation in between
-Vec3f traceParticle(RegVectorField3f field0, RegVectorField3f field1, Vec3f start, float dt);
+Vec3f traceParticle(const RegVectorField3f& field, Vec3f start, float dt);
+//time varying, field0 at time t0, field1 at time t1 = t0+dt, linear interpolation in between
+Vec3f traceParticle(const RegVectorField3f& field0, const RegVectorField3f& field1, Vec3f start, float dt);
+//as before, but t0 <= start_t < start_t+dt <= t1
+Vec3f traceParticle(const RegVectorField3f& field0, float t0, const RegVectorField3f& field1, float t1, Vec3f start, float start_t, float dt);
