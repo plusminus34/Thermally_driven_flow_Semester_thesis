f48d5968aa6c4a2e5db0dafc1fc9e695304d11af
Linus Wigger <wiggerl@student.ethz.ch>
2020-04-21 08:37:37 +0200

    Viewer shows some trajectories obtained by particle tracing

--- viewer/src/SceneWidget.cpp
+++ viewer/src/SceneWidget.cpp
@@ -6,12 +6,12 @@
 #include <vtkRenderer.h>
 #include <vtkProperty.h>
 
-#include "core//Math.hpp"
+#include "core/Math.hpp"
 #include <vtkPoints.h>
 #include <vtkPolyLine.h>
 #include <vector>
 #include "core/ParticleTracer.hpp"
-
+#include "core/ISampleField.hpp"
 
 //copypasted(mostly) from Exercise 4
 // creates an actor that draws a line in a given color
@@ -55,8 +55,31 @@ SceneWidget::SceneWidget()
 	CreateTestScene();
 }
 
+const double M_PI = 3.14159265358979;
+
+class ABCFlow : public ISampleField<Vec4f,4> {
+public:
+	virtual Vec4f Sample(const Vec<double, 4>& coord) const override {
+		return Vec4f(
+			((1 - exp(-0.1*coord[3]))*sin(2.0*M_PI*coord[3]) + sqrt(3))*sin(coord[2]) + cos(coord[1]),
+			((1 - exp(-0.1*coord[3]))*sin(2.0*M_PI*coord[3]) + sqrt(3))*cos(coord[2]) + sqrt(2)*sin(coord[0]),
+			sin(coord[1]) + sqrt(2)*cos(coord[0]),
+			1
+		);
+	}
+};
+
+class CenterField : public ISampleField<Vec2f, 2> {
+public:
+	virtual Vec2f Sample(const Vec<double, 2>& coord) const override {
+		return Vec2f( -coord[1], coord[0] );
+	}
+};
+
 void SceneWidget::CreateTestScene()
 {
+	const int sampled_field_resolution = 20;
+
 	const Vec3i res(10, 10, 10);
 	const Vec3d p1(-1, -1, -1);
 	const Vec3d p2(1, 1, 1);
@@ -72,33 +95,66 @@ void SceneWidget::CreateTestScene()
 		field.SetVertexDataAt(gridCoord, field_val);
 	}
 
+	CenterField centerField_analytic;
+	const Vec2i res2(sampled_field_resolution, sampled_field_resolution);
+	const BoundingBox2d bb2d(Vec2d(-2, -2), Vec2d(2, 2));
+	RegVectorField2f centerField_sampled(res2, bb2d);
+	for (size_t i = 0; i < res2[0] * res2[1]; ++i) {
+		Vec2i gridCoord = centerField_sampled.GetGridCoord(i);
+		Vec2d coord = centerField_sampled.GetCoordAt(gridCoord);
+		centerField_sampled.SetVertexDataAt(gridCoord, centerField_analytic.Sample(coord));
+	}
+
 	ParticleTracer<Vec3f, 3> tracer;
+
+	ParticleTracer<Vec4f, 4Ui64> tracer4;
+	ABCFlow abcFlow;
 
 	int nSteps = 128;
 	std::vector<Vec3f> dots(nSteps+1);
+
+	std::vector<Vec4f> dots2(nSteps + 1);
+	std::vector<Vec3f> dots2_vec3(dots2.size());
+	dots2[0] = Vec4f(1, 1, 1, 0);
+
+	std::vector<Vec2f> dots3(nSteps + 1);
+	std::vector<Vec3f> dots3_vec3(dots3.size());
+	dots3[0] = Vec2f(1, 0);
+	ParticleTracer<Vec2f, 2> tracer2;
+
 	Vec3f start(1, 0, 1);
 	double dt = 0.1;
 	dots[0] = (start);
 	for (int i = 0; i < nSteps; ++i) {
 		Vec3f res = tracer.traceParticle(field, dots[i], dt);
 		dots[i+1] = res;
+		dots2[i + 1] = tracer4.traceParticle(abcFlow, dots2[i], dt);
+		dots3[i + 1] = tracer2.traceParticle(centerField_analytic, dots3[i], dt);
+	}
+	for (int i = 0; i < dots2.size();++i) {
+		dots2_vec3[i] = Vec3f(dots2[i][0], dots2[i][1], dots2[i][2]);
+	}
+	for (int i = 0; i < dots3.size(); ++i) {
+		dots3_vec3[i] = Vec3f(dots3[i][0], dots3[i][1], 0.f);
 	}
 
 
 	vtkNew<vtkNamedColors> colors;
 
-	vtkNew<vtkSphereSource> sphereSource;
+//	vtkNew<vtkSphereSource> sphereSource;
 
-	vtkNew<vtkPolyDataMapper> sphereMapper;
-	sphereMapper->SetInputConnection(sphereSource->GetOutputPort());
+//	vtkNew<vtkPolyDataMapper> sphereMapper;
+//	sphereMapper->SetInputConnection(sphereSource->GetOutputPort());
 
-	vtkNew<vtkActor> sphereActor;
-	sphereActor->SetMapper(sphereMapper);
-	sphereActor->GetProperty()->SetColor(colors->GetColor4d("Tomato").GetData());
+//	vtkNew<vtkActor> sphereActor;
+//	sphereActor->SetMapper(sphereMapper);
+//	sphereActor->GetProperty()->SetColor(colors->GetColor4d("Tomato").GetData());
 
 	vtkNew<vtkRenderer> renderer;
-	renderer->AddActor(sphereActor);
+//	renderer->AddActor(sphereActor);
 	renderer->AddActor(createLineActor(dots, Vec3f(1, 0, 0)));
+	renderer->AddActor(createLineActor(dots2_vec3, Vec3f(1, 0, 1)));
+	renderer->AddActor(createLineActor(dots3_vec3, Vec3f(1, 1, 1)));
 	renderer->SetBackground(colors->GetColor3d("SteelBlue").GetData());
 
 	GetRenderWindow()->AddRenderer(renderer);
