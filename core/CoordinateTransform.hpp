c8ddaf298bad25c53f5dac53086d3e61df98b197
Wigger  Linus <wiggerl@ethz.ch>
2020-05-23 17:53:01 +0200

    Improve CoordinateTransform by using correct pole and radians

--- core/CoordinateTransform.hpp
+++ core/CoordinateTransform.hpp
@@ -1,14 +1,15 @@
 #pragma once
 
 #include <cmath>
+#include <iostream>
 
-const double TO_RAD = 3.141592653589793 / 180.0;//shouldn't that exist somewhere?
-const double POLE_PHI = 90.0;//mNorthPole.Lat;
-const double POLE_LAMBDA = 0;//mNorthPole.Lon;
-const double ZRPI18 = 57.2957795;
-const double ZPIR18 = 0.0174532925;
-const double SINPOL = sin(ZPIR18*POLE_PHI * TO_RAD);
-const double COSPOL = cos(ZPIR18*POLE_PHI * TO_RAD);
+// TODO obtain rotated north pole from .nc file?
+const double POLE_PHI = 43;// latitude of rotated north pole
+const double POLE_LAMBDA = -170;// longitude of rotated noth pole
+const double ZRPI18 = 57.2957795;// 180/PI
+const double ZPIR18 = 0.0174532925;// PI/180
+const double SINPOL = sin(ZPIR18*POLE_PHI);
+const double COSPOL = cos(ZPIR18*POLE_PHI);
 
 class CoordinateTransform {
 public:
@@ -17,17 +18,17 @@ public:
 		double zphis = ZPIR18 * rlat;
 		double zlams = rlon > 180 ? rlon - 360 : rlon;
 		zlams *= ZPIR18;
-		double arg = COSPOL * cos(zphis*TO_RAD)*cos(zlams*TO_RAD) + SINPOL * sin(zphis*TO_RAD);
+		double arg = COSPOL * cos(zphis) * cos(zlams) + SINPOL * sin(zphis);
 
 		// lat
-		lat = ZRPI18 * asin(arg) / TO_RAD;
+		lat = ZRPI18 * asin(arg);
 
 		// lon
 		double ZLAMPOL = ZPIR18 * POLE_LAMBDA;
-		const double tmp1 = sin(ZLAMPOL*TO_RAD) * (-SINPOL * cos(zlams*TO_RAD) * cos(zphis*TO_RAD) + COSPOL * sin(zphis*TO_RAD));
-		const double tmp2 = cos(ZLAMPOL*TO_RAD) * sin(zlams*TO_RAD) * cos(zphis*TO_RAD);
-		double ZARG1 = tmp1 - tmp2;
-		double ZARG2 = tmp1 + tmp2;
+		const double tmpa = -SINPOL * cos(zlams) + COSPOL * sin(zphis);
+		const double tmpb = sin(zlams)*cos(zphis);
+		double ZARG1 = sin(ZLAMPOL) * tmpa - cos(ZLAMPOL)*tmpb;
+		double ZARG2 = cos(ZLAMPOL) * tmpa - sin(ZLAMPOL)*tmpb;
 		lon = 0;
 		if (std::abs(ZARG2) < 1E-30)
 		{
@@ -39,7 +40,7 @@ public:
 				lon = -90.0;
 		}
 		else
-			lon = ZRPI18 * atan2(ZARG1, ZARG2) / TO_RAD;
+			lon = ZRPI18 * atan2(ZARG1, ZARG2);
 	}
 
 	static void LatLonToRlanRlon(double lat, double lon, double& rlat, double& rlon) {
@@ -51,13 +52,13 @@ public:
 		ZLAM = ZPIR18 * ZLAM;
 
 		// lat
-		double ZARG = COSPOL * cos(ZPHI*TO_RAD) * cos((ZLAM - ZLAMPOL) *TO_RAD) + SINPOL * sin(ZPHI*TO_RAD);
-		rlat = ZRPI18 * asin(ZARG) / TO_RAD;
+		double ZARG = COSPOL * cos(ZPHI) * cos((ZLAM - ZLAMPOL)) + SINPOL * sin(ZPHI);
+		rlat = ZRPI18 * asin(ZARG);
 
 
 		// lon
-		double ZARG1 = -sin((ZLAM - ZLAMPOL)*TO_RAD) * cos(ZPHI*TO_RAD);
-		double ZARG2 = -SINPOL * cos(ZPHI*TO_RAD) * cos((ZLAM - ZLAMPOL)*TO_RAD) + COSPOL * sin(ZPHI*TO_RAD);
+		double ZARG1 = -sin((ZLAM - ZLAMPOL)) * cos(ZPHI);
+		double ZARG2 = -SINPOL * cos(ZPHI) * cos((ZLAM - ZLAMPOL)) + COSPOL * sin(ZPHI);
 		if (std::abs(ZARG2) < 1E-30)
 		{
 			if (std::abs(ZARG1) < 1E-30)
@@ -73,18 +74,18 @@ public:
 			}
 		}
 		else
-			rlon = ZRPI18 * atan2(ZARG1, ZARG2) / TO_RAD;
+			rlon = ZRPI18 * atan2(ZARG1, ZARG2);
 	}
 
 	static void degreeLengths(double lat, double& lat_length, double& lon_length) {
 		//From https://en.wikipedia.org/wiki/Geographic_coordinate_system
 		//Length of a degree in meters
-		lat_length = 111132.92 - 559.82 * cos(2 * lat*TO_RAD) + 1.175 * cos(4 * lat*TO_RAD) - 0.0023*cos(6 * lat*TO_RAD);
-		lon_length = 111412.84 * cos(lat*TO_RAD) - 93.5 * cos(3 * lat*TO_RAD) + 0.118*cos(5 * lat*TO_RAD);
+		lat_length = 111132.92 - 559.82 * cos(2 * lat) + 1.175 * cos(4 * lat) - 0.0023*cos(6 * lat);
+		lon_length = 111412.84 * cos(lat) - 93.5 * cos(3 * lat) + 0.118*cos(5 * lat);
 	}
 
 	static void degreeLengthsSimple(double lat, double& lat_length, double& lon_length) {
-		lat_length = TO_RAD * 6367449;//*cos(0*TO_RAD)
-		lon_length = lat_length * cos(lat*TO_RAD);
+		lat_length = 6367449;//*cos(0)
+		lon_length = lat_length * cos(lat);
 	}
 };
