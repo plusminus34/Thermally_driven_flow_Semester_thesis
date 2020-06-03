cc6c3aedec496fe30084e8dab8c69c6cfe78ae4d
Wigger  Linus <wiggerl@ethz.ch>
2020-06-03 14:09:34 +0200

    Less numeric troubles in rlonrlathfield

--- core/RlonRlatHField.hpp
+++ core/RlonRlatHField.hpp
@@ -3,6 +3,7 @@
 #include "RegularGrid.hpp"
 
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -16,53 +17,37 @@ public:
 		lvl_top = hhl->GetResolution()[2] - 1;
 		dx = uvw->GetVoxelSize()[0];
 		dy = uvw->GetVoxelSize()[1];
+		x0 = uvw->GetDomain().GetMin()[0];
+		y0 = uvw->GetDomain().GetMin()[1];
+		x1 = uvw->GetDomain().GetMax()[0];
+		y1 = uvw->GetDomain().GetMax()[1];
+		res_x = uvw->GetResolution()[0];
+		res_y = uvw->GetResolution()[1];
 	}
 	~RlonRlatHField() { delete uvw; }
 
 	virtual TValueType Sample(const Vec3d& coord) const override {
-		double x0 = uvw->GetDomain().GetMin()[0];
-		double y0 = uvw->GetDomain().GetMin()[1];
-		int rlon_i = floor((coord[0] - x0) / dx);
-		int rlat_i = floor((coord[1] - y0) / dy);
-		float w0 = (coord[0] - x0 - rlon_i * dx) / dx;
-		float w1 = (coord[1] - y0 - rlat_i * dy) / dy;
-		//cout << "Sample at "<<coord[0]<<" "<<coord[1]<<" "<<coord[2]<<endl;
-		//if(w0<0||w0>1||w1<0||w1>1)
-		//cout << "  w0,w1 = " << w0 << "," << w1 << endl;
+		double rlon_d = (coord[0] - x0) / (x1 - x0) * (res_x-1);
+		double rlat_d = (coord[1] - y0) / (y1 - y0) * (res_y - 1);
+		int rlon_i = std::min(std::max(0, (int)std::floor(rlon_d)), res_x - 1);
+		int rlat_i = std::min(std::max(0, (int)std::floor(rlat_d)), res_y - 1);
+		double w0 = rlon_d - rlon_i;
+		double w1 = rlat_d - rlat_i;
 		if (w0 < 0) { cout << "ERROR: w0 = " << w0 << " < 0" << endl; w0 = 0; }
 		else if (w0 > 1) { cout << "ERROR: w0 = " << w0 << " > 1" << endl; w0 = 1; }
 		if (w1 < 0) { cout << "ERROR: w1 = " << w1 << " < 0" << endl; w1 = 0; }
 		else if (w1 > 1) { cout << "ERROR: w1 = " << w1 << " > 1" << endl; w1 = 1; }
-		//cout << "RlonRlat sample 2 for tmp0\n";
 		TValueType tmp0 = SampleXYiHd(rlon_i, rlat_i, coord[2]) * (1 - w0) + SampleXYiHd(rlon_i + 1, rlat_i, coord[2]) * w0;
-		//cout << "RlonRlat sample 2 for tmp1\n";
 		TValueType tmp1 = SampleXYiHd(rlon_i, rlat_i + 1, coord[2]) * (1 - w0) + SampleXYiHd(rlon_i + 1, rlat_i + 1, coord[2]) * w0;
-		//cout << "  tmp0 " << tmp0[0] << " " << tmp0[2] << "   tmp1 " << tmp1[0] << " " << tmp1[2] << endl;
 		return tmp0 * (1 - w1) + tmp1 * w1;
 	}
 	TValueType SampleXYiHd(int rlon_i, int rlat_i, double h) const {
-		//cout << "samplexyihd " << rlon_i << " " << rlat_i << " " << h << endl;
 		/*
 		seems	level	0		80
 				height	22000	800-something
 		*/
-		//TODO figure out why this is necessary
-		if (rlon_i < 0) {
-			//cout << "rloni = " << rlon_i << endl;
-			rlon_i = 0;
-		}
-		else if (rlon_i >= uvw->GetResolution()[0]) {
-			//cout << "rloni = " << rlon_i << endl;
-			rlon_i = uvw->GetResolution()[0] - 1;
-		}
-		if (rlat_i < 0) {
-			//cout << "rlati = " << rlat_i << endl;
-			rlat_i = 0;
-		}
-		else if (rlat_i >= uvw->GetResolution()[1]) {
-			//cout << "rlati = " << rlat_i << endl;
-			rlat_i = uvw->GetResolution()[1] - 1;
-		}
+		assert(rlon_i > -1 && rlon_i < res_x);
+		assert(rlat_i > -1 && rlat_i < res_y);
 
 		//binary search at rlon_i,rlat_i to find level for h
 		int lower = 0;
@@ -94,8 +79,8 @@ private:
 
 	Vec3i hhl_offset;
 
-	int lvl_top;
-	float dx, dy;
+	int lvl_top, res_x, res_y;
+	double x0, y0, x1, y1, dx, dy;
 };
 
 typedef RlonRlatHField<Vec3f> RlonRlatHField_Vec3f;
